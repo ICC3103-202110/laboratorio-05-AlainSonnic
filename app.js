1c146a14eebba9f5e4ac2432a1741ff44bfd97a5
alain <asonnic@miuandes.cl>
2021-05-14 18:00:42 -0400

    tabla creada

--- app.js
+++
@@ -1,1 +0,0 @@
-/* empezando la tarea*/
--- view.js
+++ view.js
@@ -1,1 +1,17 @@
-/* empezando la tarea*/
+
+
+const { printTable } = require('console-table-printer');
+
+function viewapp(ba,tipp,tip,Total){
+    const testCases = [
+        { "Bill Amount": ba, "tip (%)": tipp, "tip": tip,"Total $": Total},
+      ];
+    return printTable(testCases);
+}
+
+
+module.exports = {
+    viewapp
+}
+
+console.log(viewapp(1000,50,500,1500))
